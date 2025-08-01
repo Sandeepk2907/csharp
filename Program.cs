1fa22b81018d6af592ef24144730114cdbf0837e
Sandeepk2907 <sandeepkharwar278@gmail.com>
2025-08-01 11:45:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace odd_even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 36;
+            odd_even oe = new odd_even(n);
+            oe.display(n);
+        }
+    }
+}
