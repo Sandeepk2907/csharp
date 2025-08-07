175b5d845b05c87dbddd54af61ea3eac10fc61cc
Sandeepk2907 <sandeepkharwar278@gmail.com>
2025-08-07 23:35:12 +0530

    Add files via upload

---
+++ reverse.cs
@@ -0,0 +1,12 @@
+﻿namespace reverse
+{
+    public class reverse
+    {
+        static void Main(string[] args)
+        {
+            reverse2 r =new reverse2 ();
+            r.rev();
+            r.display();
+        }
+    }
+}
---
+++ reverse2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse
+{
+    public class reverse2
+    {
+        int number;
+        int reverse = 0;
+        public void rev()
+        {
+            Console.Write("Enter a number: ");
+            
+            number = Convert.ToInt32(Console.ReadLine());
+          
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                reverse = reverse * 10 + digit;
+                number = number / 10;
+            }
+        }
+        public void display()
+        {
+            Console.Write("Reversed Number:" + reverse);
+        }
+
+        }
+    }
