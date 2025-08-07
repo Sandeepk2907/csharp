6676170461c07318d397f6198283d812b90c8301
Sandeepk2907 <sandeepkharwar278@gmail.com>
2025-08-08 00:00:31 +0530

    Add files via upload

---
+++ SumDig.cs
@@ -0,0 +1,13 @@
+﻿namespace sum_of_digit
+{
+    public class sum_digit
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers:");
+            sum_dig s = new sum_dig();
+            s.sum();
+            s.display();
+        }
+    }
+}
---
+++ sum_dig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_digit
+{
+    public class sum_dig
+    {
+       
+        int num = Convert.ToInt32(Console.ReadLine());
+        int s = 0;
+        public void sum()
+        {
+             int temp = num;
+            while (temp != 0)
+            {
+                int digit = temp % 10;
+                s += digit;
+                temp /= 10;
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("Sum of digits: " + s);
+        }
+    }
+}
+    
