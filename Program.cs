db8657da3c89aee56a60df1739c8d5e9bc33ee00
Sandeepk2907 <sandeepkharwar278@gmail.com>
2025-08-07 22:38:20 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
-﻿namespace ConsoleApp2
+﻿namespace person
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             
-           numbers number = new numbers();
-            number.num(6);
-            number.num(0);
-            number.num(-5);
-           
+            person p=new person();
+            p.names();
+            p.display();
         }
     }
 }
---
+++ person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person
+{
+    public class person
+    {
+        string name;
+        public void names()
+        {
+            
+            Console.WriteLine("Enter Name:");
+            name = Console.ReadLine();
+        }
+        public void display()
+        {
+            Console.WriteLine("Your Name:" + name);
+        }
+
+    
+    }
+}
