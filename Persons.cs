e25c2c4494f9346d33e481a1d11cb1a34569f61f
Sandeepk2907 <sandeepkharwar278@gmail.com>
2025-08-07 22:43:41 +0530

    Add files via upload

---
+++ Persons.cs
@@ -0,0 +1,13 @@
+﻿namespace person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            person p=new person();
+            p.names();
+            p.display();
+        }
+    }
+}
