4bf4090d818f4060740105d37cf89ba6b82084f1
Sandeepk2907 <sandeepkharwar278@gmail.com>
2025-08-08 12:38:42 +0530

    Add files via upload

---
+++ encap.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    public class encap
+    {
+        public void de()
+        { }
+            private string name;
+        private int age;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Age Should be between 0 And 100");
+                }
+            }
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"Name :{name}");
+            Console.WriteLine($"Age:{age}");
+        }
+    }
+
+  }
+
+
+
+
---
+++ encap1.cs
@@ -0,0 +1,13 @@
+﻿namespace encapsulation
+{
+    internal class Encapsulation
+    {
+        static void Main(string[] args)
+        {
+            encap e=new encap();
+            e.Name="Sandeep";
+            e.Age=18;
+            e.display();
+        }
+    }
+}
